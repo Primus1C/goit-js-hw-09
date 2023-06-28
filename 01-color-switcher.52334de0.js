87f31fa10e262801051ded48aba92fe13c62ada6
Primus1C <110535829+Primus1C@users.noreply.github.com>
2023-06-28 12:26:23 +0000

    Deploying to gh-pages from @ Primus1C/goit-js-hw-09@a9ecdfa1881e4b2e2be7eec111737c4825de789a 🚀

---
+++ 01-color-switcher.52334de0.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]").addEventListener("click",(function(t){console.log(t.target)}));document.querySelector("[data-stop]").addEventListener("click",(function(t){console.log(t.target)}));
+//# sourceMappingURL=01-color-switcher.52334de0.js.map
---
+++ 01-color-switcher.b83927b3.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]").addEventListener("click",(function(t){console.log(t.target)})),document.querySelector("[data-stop]").addEventListener("click",(function(t){console.log(t.target)}));
+//# sourceMappingURL=01-color-switcher.b83927b3.js.map
