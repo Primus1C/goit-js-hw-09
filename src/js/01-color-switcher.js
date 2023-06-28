a9ecdfa1881e4b2e2be7eec111737c4825de789a
Artur Rakhimov <kharkiv1c@gmail.com>
2023-06-28 14:26:48 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+const startBtn = document.querySelector('[data-start]');
+startBtn.addEventListener('click', onClickStart);
+const stopBtn = document.querySelector('[data-stop]');
+stopBtn.addEventListener('click', onClickStop);
 
+function onClickStart(evt) {
+  console.log(evt.target);
+}
+function onClickStop(evt) {
+  console.log(evt.target);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
