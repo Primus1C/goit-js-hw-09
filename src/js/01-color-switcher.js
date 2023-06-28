41e6d74e27bc6241b35afff8958eacd032942c46
Artur Rakhimov <kharkiv1c@gmail.com>
2023-06-28 19:02:25 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,12 +2,33 @@ const startBtn = document.querySelector('[data-start]');
 startBtn.addEventListener('click', onClickStart);
 const stopBtn = document.querySelector('[data-stop]');
 stopBtn.addEventListener('click', onClickStop);
+let id = 0;
+
+setActiveButton();
 
 function onClickStart(evt) {
-  console.log(evt.target);
+  //console.log(evt.target);
+  setBckgColor();
+  setActiveButton(false);
+
+  id = setInterval(function () {
+    setBckgColor();
+  }, 1000);
 }
 function onClickStop(evt) {
-  console.log(evt.target);
+  //console.log(evt.target);
+  clearInterval(id);
+  setActiveButton();
+}
+
+function setBckgColor() {
+  document.body.style.background = getRandomHexColor();
+}
+
+function setActiveButton(start = true) {
+  startBtn.disabled = !start;
+  stopBtn.disabled = start;
+  //console.log(startBtn);
 }
 
 function getRandomHexColor() {
