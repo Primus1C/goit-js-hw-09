69d8618976139805507cd12e058c147798e90245
Artur <110535829+Primus1C@users.noreply.github.com>
2023-06-29 16:44:17 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,24 @@
 import Notiflix from 'notiflix';
 
+const refs = {
+  form: document.querySelector('.form'),
+  delayArea: document.getElementsByName('delay'),
+  stepArea: document.getElementsByName('step'),
+  amountArea: document.getElementsByName('amount'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
 function onFormSubmit(evt) {
-  
-  
+  console.log(999);
+  //Notiflix.Notify.failure(amount);
 }
 
-function createPromise(position, delay) {
+/* function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
   } else {
     // Reject
   }
-}
+} */
